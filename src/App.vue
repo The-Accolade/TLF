4b2479a1c638c8af20b9f57157fc610089c580f9
Fiyin-vee <101519721+Fiyin-vee@users.noreply.github.com>
2023-01-07 22:43:54 +0100

    Setup done

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="bg-red-500 h-screen w-full">
+      <h1 class="text-white font-black text-3xl">Hello World</h1>
+    </div>
+  </div>
+</template>
+<script setup>
+</script>
